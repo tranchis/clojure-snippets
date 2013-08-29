b06db08c4eb730ec70ec7e2a81687181e843a5d6
Sergio Alvarez-Napagao <tranchis@gmail.com>
2013-08-29 20:41:52 +0200

    0.0.2: load-props

--- src/main/clojure/snippets_generic.clj
+++ src/main/clojure/snippets_generic.clj
@@ -1,6 +1,7 @@
 (ns snippets-generic)
 
 (defn load-props
+  "Receives a path and loads the Java properties for the file represented by the path inside the classpath (typically, a resource)."
   [file-name]
   (with-open [^java.io.Reader reader (clojure.java.io/reader (clojure.java.io/resource file-name))] 
     (let [props (java.util.Properties.)]
